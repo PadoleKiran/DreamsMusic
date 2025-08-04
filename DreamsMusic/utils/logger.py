f2887406d82858c627786076b7433221676a4331
DreamsRobot <hdhindihub@gmail.com>
2025-08-04 14:55:42 +0530

    Update logger.py

--- DreamsMusic/utils/logger.py
+++ DreamsMusic/utils/logger.py
@@ -2,3 +2,15 @@ import logging
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger("DreamsMusic")
+
+# Logger setup
+LOGGER = logging.getLogger("DreamsMusic")
+LOGGER.setLevel(logging.INFO)
+
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(formatter)
+
+if not LOGGER.hasHandlers():
+    LOGGER.addHandler(console_handler)
