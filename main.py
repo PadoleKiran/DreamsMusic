1f46cad831c9561d559b54b3b6d9af8dd012c5a7
DreamsRobot <hdhindihub@gmail.com>
2025-08-04 13:41:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
+from pyrogram import Client
 from DreamsMusic import config
-from pyrogram import Client, idle
-from DreamsMusic.core.call import start_streaming
+from DreamsMusic.utils.logger import LOGGER
+from DreamsMusic.core.call import DreamsPlayer
+
+from DreamsMusic.handlers import (
+    start,
+    play,
+    admin,
+    auth,
+    extra,
+    broadcast,
+    stats
+)
 
 app = Client(
     "DreamsMusic",
     api_id=config.API_ID,
     api_hash=config.API_HASH,
-    bot_token=config.BOT_TOKEN
+    bot_token=config.BOT_TOKEN,
+    plugins={"root": "DreamsMusic/handlers"},
 )
 
-async def main():
-    await app.start()
-    await start_streaming()
-    print("DreamsMusic Bot Started.")
-    await idle()
+Dreams = DreamsPlayer(app)
 
 if __name__ == "__main__":
-    import asyncio
-    asyncio.run(main())
+    LOGGER.info("Starting DreamsMusic Bot...")
+    app.start()
+    Dreams.start()
+    LOGGER.info("DreamsMusic Bot Started Successfully.")
+    app.idle()
